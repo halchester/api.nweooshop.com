0f9428ba3764402e14f14f91e5458fddc75168a6
halchester <chester.htooaunglinn@gmail.com>
2021-07-07 16:23:21 +0630

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import path from "path";
 import mongoose from "mongoose";
 import bluebird from "bluebird";
 import { MONGODB_URI, SESSION_SECRET } from "./utils/secrets";
+import cors from 'cors'
 
 // Create Express server
 const app = express();
@@ -33,6 +34,7 @@ mongoose
 app.set("port", process.env.PORT || 8000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 app.use(
   session({
     resave: true,
